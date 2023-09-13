fe1af1dd9797c4953c835429d3be0232bae88f9a
Alyona Dudina <badyukiko@gmail.com>
2023-09-13 09:26:19 +0300

    create enrolling APIView

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
-from courses.models import Subject
+from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class SubjectListView(generics.ListAPIView):
@@ -11,7 +14,15 @@ class SubjectListView(generics.ListAPIView):
 
 class SubjectDetailView(generics.RetrieveAPIView):
     """Retrieves a subject by pk."""
+
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
 
+class CourseEnrollView(APIView):
+    """Enrolls a patient in the current course."""
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
